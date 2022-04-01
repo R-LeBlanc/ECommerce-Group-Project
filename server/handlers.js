efda38d7c5943017cda8f10fc3d30efcec650251
Beks <guess777@gmail.com>
2022-04-01 11:18:17 -0400

    get products endpoints created

---
+++ server/handlers.js
@@ -0,0 +1,45 @@
+"use strict";
+
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Retrieves all products from the database
+const getProducts = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  await client.connect();
+  const db = client.db("ReservoirCats");
+
+  db.collection("Products")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(500).json({ status: 500, message: err });
+      }
+      res.status(200).json({ status: 200, data: result });
+      client.close();
+    });
+};
+
+// Retrieves a product based on the id
+const getProductById = async (req, res) => {
+  const _id = req.params._id;
+  const client = new MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("ReservoirCats");
+
+    const product = await db.collection("Products").findOne({ _id });
+    res.status(200).json({ status: 200, data: product });
+  } catch (err) {
+    res.status(500).json({ status: 500, message: err.message });
+  }
+  client.close();
+};
+
+module.exports = { getProducts, getProductById };
