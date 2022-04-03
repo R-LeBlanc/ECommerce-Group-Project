50f40e06a3d3d65bb36403aed6de34fd9cea0baa
Beks <guess777@gmail.com>
2022-04-03 11:11:48 -0400

    batch import of companies

--- server/batchImport.js
+++ server/batchImport.js
@@ -27,4 +27,22 @@ const batchImport = async () => {
   item.close();
 };
 
-batchImport();
+// Imports the companies data from the data file
+const companies = JSON.parse(fs.readFileSync("data/companies.json"));
+
+const companiesImport = async () => {
+  const client = new MongoClient(MONGO_URI, options);
+
+  try {
+    await client.connect();
+    const db = client.db("ReservoirCats");
+    await db.collection("Companies").insertMany(companies);
+    console.log(companies);
+  } catch (err) {
+    console.log(err);
+  }
+  console.log("success!");
+  client.close();
+};
+
+// batchImport();
